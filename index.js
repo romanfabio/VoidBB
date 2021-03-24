05e7fa939032f99e4b89351bc2d126966ccf69d9
Fabio Roman <romanfabio@protonmail.com>
2021-03-24 14:01:49 +0100

    Add Mongoose, add post model

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fastify = require('fastify');
+const mongoose = require('mongoose');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
 let app = fastify({logger: true});
+
+mongoose.connect('mongodb://localhost:27017/voidbb', {useNewUrlParser: true, useUnifiedTopology: true}).catch(err => console.log(err));
 
 app.register(require('fastify-formbody'));
 

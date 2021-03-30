b91f5d793642231e6d67ab10c51221b14a0035f8
Fabio Roman <romanfabio@protonmail.com>
2021-03-30 22:00:25 +0200

    Integrate database in home controller

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,13 +2,15 @@ const db = require('../database/db');
 
 module.exports = {
     get: (request, reply) => {
-        const auth = request.session.get('username');
 
         const userModel = db.getUserModel();
 
         userModel.findAll().then((value) => {
-
-            reply.view('home.ejs', {title: 'Home', styles: ['home.css'], topics: [{title: 'Ciao', description: "Come va?"}],users: value});
+            const auth = request.session.get('username');
+            if(auth)
+                reply.view('home.ejs', {title: 'Home', styles: ['home.css'], users: value, auth: auth});
+            else
+                reply.view('home.ejs', {title: 'Home', styles: ['home.css'], users: value});
         }, (err) => {
             reply.view('home.ejs', {title: 'Home', styles: ['home.css'], error: 'An error has occured, retry later'});
         });

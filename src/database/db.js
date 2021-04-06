5328f119c52caf487cd9af6320ebcb510bb8bcf3
Fabio Roman <romanfabio@protonmail.com>
2021-04-06 13:53:55 +0200

    Promisify database's init method v2

--- src/database/db.js
+++ src/database/db.js
@@ -7,15 +7,8 @@ const PostModel = require('./models/post');
 const sequelize = new Sequelize('postgres://voidbbuser:voidbbuser@localhost:5432/voidbb');
 
 module.exports = {
-    init: async () => {
-
-        try {
-            await sequelize.authenticate();
-            console.log("[ OK ] Database");
-        } catch(err) {
-            console.log("Can't connect to database");
-        }
-
+    init: () => {
+        
         sequelize.authenticate()
             .then(() => UserModel(sequelize))
             .then(() => ForumModel(sequelize))

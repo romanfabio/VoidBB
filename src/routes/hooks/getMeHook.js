23b8025444c770e4c4dce7eb66e6863aa21d9ee6
Fabio Roman <romanfabio@protonmail.com>
2021-04-20 22:08:02 +0200

    Rewrite getMeHook using async/await

--- src/routes/hooks/getMeHook.js
+++ src/routes/hooks/getMeHook.js
@@ -11,64 +11,63 @@ const cache = require('../../util/cache');
     Non-Authenticated User
         - request.user_global_group : number = <Anonymous global group id>
 */
-module.exports = (request, reply, done) => {
+module.exports = async (request, reply) => {
     console.log('GetMe Hook');
 
     request.view_params = {};
 
     const username = request.session.get('username');
-    if(username) {
 
-        const msgs = reply.flash();
+    if (!username) {
+        request.user_global_group = pex.GLOBAL_ANONYMOUS;
+        return;
+    }
 
-        if(msgs) {
-            if(msgs.info)
-                request.view_params.INFO = msgs.info[0];
-            if(msgs.error)
-                request.view_params.ERROR = msgs.error[0];
-        }
+    const msgs = reply.flash();
 
-        const cached = cache.global_group(username);
+    if (msgs) {
+        if (msgs.info)
+            request.view_params.INFO = msgs.info[0];
+        if (msgs.error)
+            request.view_params.ERROR = msgs.error[0];
+    }
 
-        if(cached) {
-            request.is_auth = username;
-            request.view_params.USERNAME = username;
-            request.user_global_group = cached;
+    const cached = cache.global_group(username);
 
-            if(cached == pex.GLOBAL_ADMIN)
-                request.view_params.AP = true;
-            
-            done();
+    let global_group;
+
+    if (cached) {
+        global_group = cached;
+    } else {
+        const UserModel = db.getUserModel();
+
+        try {
+            const user = await UserModel.findByPk(username, { attributes: ['global_group'] });
+
+            if (user === null) {
+                request.user_global_group = pex.GLOBAL_ANONYMOUS;
+                request.session.delete();
+
+                return;
+            } else {
+                global_group = user.global_group;
+
+                cache.invalidate_global_group(username, user.global_group);
+            }
+        } catch (err) {
+            console.log(err);
+            request.user_global_group = pex.GLOBAL_ANONYMOUS;
+
             return;
         }
 
-        const UserModel = db.getUserModel();
-
-        UserModel.findByPk(username, {attributes: ['global_group']})
-            .then((user) => {
-                if(user === null) {
-                    request.user_global_group = pex.GLOBAL_ANONYMOUS;
-                    request.session.delete();
-                    done();
-                } else {
-                    request.is_auth = username;
-                    request.view_params.USERNAME = username;
-                    request.user_global_group = user.global_group;
-
-                    if(user.global_group == pex.GLOBAL_ADMIN)
-                        request.view_params.AP = true;
-
-                    cache.invalidate_global_group(username, user.global_group);
-                    
-                    done();
-                }
-            }, (err) => {
-                console.log(err);
-                request.user_global_group = pex.GLOBAL_ANONYMOUS;
-                done();
-            });
-    } else {
-        request.user_global_group = pex.GLOBAL_ANONYMOUS;
-        done();
     }
-}
+
+    request.is_auth = username
+    request.view_params.USERNAME = username;
+    request.user_global_group = global_group;
+
+    if (global_group === pex.GLOBAL_ADMIN)
+        request.view_params.AP = true;
+
+};
